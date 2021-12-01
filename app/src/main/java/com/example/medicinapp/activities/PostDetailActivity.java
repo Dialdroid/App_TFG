c173b592905eb8261feebb0fd7908607931dd266
Diego <aguirre.dial@gmail.com>
2021-12-01 19:58:12 +0100

    Ahora los comentarios se suben a la base de datos

--- app/src/main/java/com/example/medicinapp/activities/PostDetailActivity.java
+++ app/src/main/java/com/example/medicinapp/activities/PostDetailActivity.java
@@ -1,22 +1,34 @@
 package com.example.medicinapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.medicinapp.R;
 import com.example.medicinapp.adapters.SliderAdapter;
+import com.example.medicinapp.models.Comment;
 import com.example.medicinapp.models.SliderItem;
+import com.example.medicinapp.providers.AuthProvider;
+import com.example.medicinapp.providers.CommentsProvider;
 import com.example.medicinapp.providers.PostProvider;
 import com.example.medicinapp.providers.UserProvider;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -24,6 +36,7 @@ import com.smarteist.autoimageslider.SliderView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -35,6 +48,8 @@ public class PostDetailActivity extends AppCompatActivity {
     List<SliderItem> mSliderItems = new ArrayList<>();
     PostProvider mPostProvider;
     UserProvider mUsersProvider;
+    CommentsProvider mCommentsProvider;
+    AuthProvider mAuthProvider;
 
     String mExtraPostId;
 
@@ -44,6 +59,7 @@ public class PostDetailActivity extends AppCompatActivity {
     CircleImageView mCircleImageViewProfile;
     Button mButtonShowProfile;
     CircleImageView mCircleImageViewBack;
+    FloatingActionButton mFabComment;
 
     String mIdUser = "";
 
@@ -59,13 +75,24 @@ public class PostDetailActivity extends AppCompatActivity {
         mCircleImageViewProfile = findViewById(R.id.circleImageProfile);
         mButtonShowProfile = findViewById(R.id.btnShowProfile);
         mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mFabComment = findViewById(R.id.fabComment);
 
         mPostProvider = new PostProvider();
         mUsersProvider = new UserProvider();
+        mCommentsProvider = new CommentsProvider();
+        mAuthProvider = new AuthProvider();
+
 
         mExtraPostId = getIntent().getStringExtra("id");
 
         getPost();
+
+        mFabComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogComment();
+            }
+        });
 
         mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +108,74 @@ public class PostDetailActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showDialogComment() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(PostDetailActivity.this);
+        alert.setTitle("¡COMENTARIO!");
+        alert.setMessage("Ingresa tu comentario");
+
+        final EditText editText = new EditText(PostDetailActivity.this);
+        editText.setHint("Texto");
+
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+          LinearLayout.LayoutParams.MATCH_PARENT,
+          LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(36,0,36,36);
+        editText.setLayoutParams(params);
+
+        RelativeLayout container = new RelativeLayout(PostDetailActivity.this);
+        RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(
+          RelativeLayout.LayoutParams.MATCH_PARENT,
+          RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        container.setLayoutParams(relativeParams);
+        container.addView(editText);
+
+        alert.setView(container);
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String value = editText.getText().toString();
+                if (!value.isEmpty()){
+                createComment(value);
+                } else {
+                    Toast.makeText(PostDetailActivity.this, "Debes ingresar el comentario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        alert.show();
+    }
+
+    private void createComment(final String value) {
+        Comment comment = new Comment();
+        comment.setComment(value);
+        comment.setIdPost(mExtraPostId);
+        comment.setIdUser(mAuthProvider.getUID());
+        comment.setTimestamp(new Date().getTime());
+        mCommentsProvider.create(comment).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(PostDetailActivity.this, "El comentario se creo correctamente", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(PostDetailActivity.this, "No se pudo crear el comentario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
 
     private void goToShowProfile() {
         if(!mIdUser.equals("")){
