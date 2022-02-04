d120b76b6593edea78e223b1261ef972d0e9cf1d
Diego <aguirre.dial@gmail.com>
2022-02-04 16:36:17 +0100

    Ya se puede seleccionar una fecha en la actividad de Bitacora

--- app/src/main/java/com/example/medicinapp/activities/BitacoraActivity.java
+++ app/src/main/java/com/example/medicinapp/activities/BitacoraActivity.java
@@ -2,15 +2,50 @@ package com.example.medicinapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.medicinapp.R;
 
+import java.util.Calendar;
+
+
 public class BitacoraActivity extends AppCompatActivity {
+
+    EditText mSelectDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitacora);
+        mSelectDate = findViewById(R.id.editTextSelectDate);
+        mSelectDate.setFocusable(false);
+        mSelectDate.setKeyListener(null);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        mSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        BitacoraActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        mSelectDate.setText(date);
+                    }
+                }, year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
     }
 }
