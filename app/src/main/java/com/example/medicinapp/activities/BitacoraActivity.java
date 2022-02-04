0860e6bda87fcc8ea18b84fcbfad617404d0c60d
Diego <aguirre.dial@gmail.com>
2022-02-04 17:48:35 +0100

    agregado selección de emociones en la bitacora
    agregadas 5 emociones básicas en la bitacora

--- app/src/main/java/com/example/medicinapp/activities/BitacoraActivity.java
+++ app/src/main/java/com/example/medicinapp/activities/BitacoraActivity.java
@@ -3,8 +3,10 @@ package com.example.medicinapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,9 +16,14 @@ import com.example.medicinapp.R;
 import java.util.Calendar;
 
 
-public class BitacoraActivity extends AppCompatActivity {
+public class BitacoraActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mSelectDate;
+    Button mBtnHappy;
+    Button mBtnBored;
+    Button mBtnSad;
+    Button mBtnAngry;
+    Button mBtnScared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,12 @@ public class BitacoraActivity extends AppCompatActivity {
         mSelectDate = findViewById(R.id.editTextSelectDate);
         mSelectDate.setFocusable(false);
         mSelectDate.setKeyListener(null);
+        mBtnHappy = findViewById(R.id.btnHappy);
+        mBtnBored = findViewById(R.id.btnBored);
+        mBtnSad = findViewById(R.id.btnSad);
+        mBtnAngry = findViewById(R.id.btnAngry);
+        mBtnScared = findViewById(R.id.btnScared);
+
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
@@ -47,5 +60,115 @@ public class BitacoraActivity extends AppCompatActivity {
             }
         });
 
+        mBtnHappy.setOnClickListener(this);
+        mBtnBored.setOnClickListener(this);
+        mBtnSad.setOnClickListener(this);
+        mBtnAngry.setOnClickListener(this);
+        mBtnScared.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnHappy:
+                Toast.makeText(this, "Estás feliz", Toast.LENGTH_SHORT).show();
+                mBtnHappy.setBackgroundTintList(null);
+                mBtnHappy.setBackgroundResource(R.drawable.ic_confident);
+
+                mBtnBored.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnBored.setBackgroundResource(R.drawable.ic_bored_white);
+
+                mBtnSad.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnSad.setBackgroundResource(R.drawable.ic_sad_white);
+
+                mBtnAngry.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnAngry.setBackgroundResource(R.drawable.ic_angry_white);
+
+                mBtnScared.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnScared.setBackgroundResource(R.drawable.ic_scared_white);
+
+                break;
+
+            case R.id.btnBored:
+                Toast.makeText(this, "Estás aburrido", Toast.LENGTH_SHORT).show();
+
+                mBtnHappy.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnHappy.setBackgroundResource(R.drawable.ic_confident_white);
+
+                mBtnBored.setBackgroundTintList(null);
+                mBtnBored.setBackgroundResource(R.drawable.ic_bored);
+
+                mBtnSad.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnSad.setBackgroundResource(R.drawable.ic_sad_white);
+
+                mBtnAngry.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnAngry.setBackgroundResource(R.drawable.ic_angry_white);
+
+                mBtnScared.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnScared.setBackgroundResource(R.drawable.ic_scared_white);
+
+                break;
+
+            case R.id.btnSad:
+                Toast.makeText(this, "Estás triste", Toast.LENGTH_SHORT).show();
+
+                mBtnHappy.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnHappy.setBackgroundResource(R.drawable.ic_confident_white);
+
+                mBtnBored.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnBored.setBackgroundResource(R.drawable.ic_bored_white);
+
+                mBtnSad.setBackgroundTintList(null);
+                mBtnSad.setBackgroundResource(R.drawable.ic_sad);
+
+                mBtnAngry.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnAngry.setBackgroundResource(R.drawable.ic_angry_white);
+
+                mBtnScared.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnScared.setBackgroundResource(R.drawable.ic_scared_white);
+
+                break;
+
+            case R.id.btnAngry:
+                Toast.makeText(this, "Estás enojado", Toast.LENGTH_SHORT).show();
+
+                mBtnHappy.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnHappy.setBackgroundResource(R.drawable.ic_confident_white);
+
+                mBtnBored.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnBored.setBackgroundResource(R.drawable.ic_bored_white);
+
+                mBtnSad.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnSad.setBackgroundResource(R.drawable.ic_sad_white);
+
+                mBtnAngry.setBackgroundTintList(null);
+                mBtnAngry.setBackgroundResource(R.drawable.ic_angry);
+
+                mBtnScared.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnScared.setBackgroundResource(R.drawable.ic_scared_white);
+
+                break;
+
+            case R.id.btnScared:
+                Toast.makeText(this, "Estás asustado", Toast.LENGTH_SHORT).show();
+
+                mBtnHappy.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnHappy.setBackgroundResource(R.drawable.ic_confident_white);
+
+                mBtnBored.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnBored.setBackgroundResource(R.drawable.ic_bored_white);
+
+                mBtnSad.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnSad.setBackgroundResource(R.drawable.ic_sad_white);
+
+                mBtnAngry.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                mBtnAngry.setBackgroundResource(R.drawable.ic_angry_white);
+
+                mBtnScared.setBackgroundTintList(null);
+                mBtnScared.setBackgroundResource(R.drawable.ic_scared);
+
+                break;
+        }
     }
 }
