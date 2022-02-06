755191e1d0ba554f8f790ffb5a693fd5a9a937e6
Diego <aguirre.dial@gmail.com>
2022-02-06 23:36:00 +0100

    cambios menores

--- app/src/main/java/com/example/medicinapp/adapters/MessagesAdapter.java
+++ app/src/main/java/com/example/medicinapp/adapters/MessagesAdapter.java
@@ -31,81 +31,81 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessagesAdapter extends FirestoreRecyclerAdapter<Message, MessagesAdapter.ViewHolder> {
 
-    Context context;
-    UserProvider mUsersProvider;
-    AuthProvider mAuthProvider;
+        Context context;
+        UserProvider mUsersProvider;
+        AuthProvider mAuthProvider;
 
-    public MessagesAdapter(FirestoreRecyclerOptions<Message> options, Context context) {
-        super(options);
-        this.context = context;
-        mUsersProvider = new UserProvider();
-        mAuthProvider = new AuthProvider();
-    }
-
-    @Override
-    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull final Message message) {
-        DocumentSnapshot document = getSnapshots().getSnapshot(position);
-        final String messageId = document.getId();
-        holder.textViewMessage.setText(message.getMessage());
-
-        String relativeTime = RelativeTime.getTimeAgo(message.getTimestamp(), context);
-        holder.textViewDate.setText(relativeTime);
-
-        if (message.getIdSender().equals(mAuthProvider.getUID())) {
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-            );
-            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            params.setMargins(150, 0, 0,0);
-            holder.linearLayoutMessage.setLayoutParams(params);
-            holder.linearLayoutMessage.setPadding(30, 20, 25, 20);
-            holder.linearLayoutMessage.setBackground(context.getResources().getDrawable(R.drawable.rounded_linear_layout));
-            holder.imageViewViewed.setVisibility(View.VISIBLE);
-            holder.textViewMessage.setTextColor(Color.WHITE);
-            holder.textViewDate.setTextColor(Color.LTGRAY);
-        }
-        else {
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-            );
-            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-            params.setMargins(0, 0, 150,0);
-            holder.linearLayoutMessage.setLayoutParams(params);
-            holder.linearLayoutMessage.setPadding(30, 20, 30, 20);
-            holder.linearLayoutMessage.setBackground(context.getResources().getDrawable(R.drawable.rounded_linear_layout_grey));
-            holder.imageViewViewed.setVisibility(View.GONE);
-            holder.textViewMessage.setTextColor(Color.DKGRAY);
-            holder.textViewDate.setTextColor(Color.LTGRAY);
+        public MessagesAdapter(FirestoreRecyclerOptions<Message> options, Context context) {
+            super(options);
+            this.context = context;
+            mUsersProvider = new UserProvider();
+            mAuthProvider = new AuthProvider();
         }
 
+        @Override
+        protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull final Message message) {
+            DocumentSnapshot document = getSnapshots().getSnapshot(position);
+            final String messageId = document.getId();
+            holder.textViewMessage.setText(message.getMessage());
+
+            String relativeTime = RelativeTime.getTimeAgo(message.getTimestamp(), context);
+            holder.textViewDate.setText(relativeTime);
+
+            if (message.getIdSender().equals(mAuthProvider.getUID())) {
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                params.setMargins(150, 0, 0,0);
+                holder.linearLayoutMessage.setLayoutParams(params);
+                holder.linearLayoutMessage.setPadding(30, 20, 25, 20);
+                holder.linearLayoutMessage.setBackground(context.getResources().getDrawable(R.drawable.rounded_linear_layout));
+                holder.imageViewViewed.setVisibility(View.VISIBLE);
+                holder.textViewMessage.setTextColor(Color.WHITE);
+                holder.textViewDate.setTextColor(Color.LTGRAY);
+            }
+            else {
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                params.setMargins(0, 0, 150,0);
+                holder.linearLayoutMessage.setLayoutParams(params);
+                holder.linearLayoutMessage.setPadding(30, 20, 30, 20);
+                holder.linearLayoutMessage.setBackground(context.getResources().getDrawable(R.drawable.rounded_linear_layout_grey));
+                holder.imageViewViewed.setVisibility(View.GONE);
+                holder.textViewMessage.setTextColor(Color.DKGRAY);
+                holder.textViewDate.setTextColor(Color.LTGRAY);
+            }
 
 
-    }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_message, parent, false);
-        return new ViewHolder(view);
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textViewMessage;
-        TextView textViewDate;
-        ImageView imageViewViewed;
-        LinearLayout linearLayoutMessage;
-        View viewHolder;
-
-        public ViewHolder(View view) {
-            super(view);
-            textViewMessage = view.findViewById(R.id.textViewMessage);
-            textViewDate = view.findViewById(R.id.textViewDateMessage);
-            imageViewViewed = view.findViewById(R.id.imageViewViewedMessage);
-            linearLayoutMessage = view.findViewById(R.id.linearLayoutMessage);
-            viewHolder = view;
         }
-    }
 
-}
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_message, parent, false);
+            return new ViewHolder(view);
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textViewMessage;
+            TextView textViewDate;
+            ImageView imageViewViewed;
+            LinearLayout linearLayoutMessage;
+            View viewHolder;
+
+            public ViewHolder(View view) {
+                super(view);
+                textViewMessage = view.findViewById(R.id.textViewMessage);
+                textViewDate = view.findViewById(R.id.textViewDateMessage);
+                imageViewViewed = view.findViewById(R.id.imageViewViewedMessage);
+                linearLayoutMessage = view.findViewById(R.id.linearLayoutMessage);
+                viewHolder = view;
+            }
+        }
+
+    }
