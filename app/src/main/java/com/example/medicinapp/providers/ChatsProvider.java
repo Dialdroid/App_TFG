556edaab9a7019ea0fea86e465616140e53c8af9
Diego <aguirre.dial@gmail.com>
2022-02-06 20:10:57 +0100

    agregada funcionalidad del chat

--- app/src/main/java/com/example/medicinapp/providers/ChatsProvider.java
+++ app/src/main/java/com/example/medicinapp/providers/ChatsProvider.java
@@ -3,6 +3,7 @@ package com.example.medicinapp.providers;
 import com.example.medicinapp.models.Chat;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class ChatsProvider {
     CollectionReference mCollection;
@@ -14,5 +15,8 @@ public class ChatsProvider {
     public void create(Chat chat) {
         mCollection.document(chat.getIdUser1()).collection("Users").document(chat.getIdUser2()).set(chat);
         mCollection.document(chat.getIdUser2()).collection("Users").document(chat.getIdUser1()).set(chat);
+    }
+    public Query getAll(String idUser) {
+        return mCollection.document(idUser).collection("Users");
     }
 }
