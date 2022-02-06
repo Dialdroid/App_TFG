e2c6df811ceb927d854fe7bb1800dc02769eede3
Diego <aguirre.dial@gmail.com>
2022-02-06 14:34:44 +0100

    Ya se pueden enviar notifiacaciones a un dispositivo físico

--- app/src/main/java/com/example/medicinapp/service/MyFirebaseMessagingClient.java
+++ app/src/main/java/com/example/medicinapp/service/MyFirebaseMessagingClient.java
@@ -1,11 +1,15 @@
 package com.example.medicinapp.service;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
+import com.example.medicinapp.channel.NotificationHelper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingClient extends FirebaseMessagingService {
 
@@ -17,6 +21,18 @@ public class MyFirebaseMessagingClient extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Map<String, String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String body = data.get("body");
+        if (title != null) {
+            showNotification(title, body);
+        }
+    }
+
+    private void showNotification(String title, String body){
+        NotificationHelper notificationHelper = new NotificationHelper(getBaseContext());
+        NotificationCompat.Builder builder = notificationHelper.getNotification(title, body);
+        notificationHelper.getManager().notify(1, builder.build());
     }
 
 }
